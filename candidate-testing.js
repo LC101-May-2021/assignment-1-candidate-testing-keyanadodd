e554e58f6a77246d25df123e68345f66f29ce4d2
keyanadodd <84354689+keyanadodd@users.noreply.github.com>
2021-06-09 23:28:05 +0000

    I finished it

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,17 +7,19 @@ let candidateName = input.question("Candidate Name: ");
 
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = ["Who was the first American woman in space?",
+let questions = ["Who was the first American woman in space?",
  "True or false: 5 kilometer == 5000 meters?", 
  "(5 + 3)/2 * 10 = ?", 
  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
  "What is the minimum crew size for the ISS? "
  ];
 
-let correctAnswer = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswer = [];
-
-
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
+let question = "";
+let correctAnswer = "";
+let candidateAnswer = "";
+let totalCorrect = 0;
 
 function askForName() {
  
@@ -28,25 +30,35 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
 for (let i = 0; i < questions.length; i++) {
-   console.log(questions[i]);
+  let answer = input.question(questions[i]);
+  candidateAnswers.push(answer);
+  //console.log(candidateAnswers)
 }
-
-
-
-
-
-
-
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+for (let i = 0; i < questions.length; i++) {
+  if(candidateAnswers[i].toUpperCase()=== correctAnswers[i].toUpperCase()) {
+    totalCorrect++
+  }
+  console.log(`${questions[i]}`)
+  console.log(`Your Answer: ${candidateAnswers[i]}`)
+  console.log(`Correct Answer: ${correctAnswers[i]}`)
+  console.log("\n")
+}
 
-
-  let grade;
+  let grade = (totalCorrect / questions.length)*100;
+  console.log (`>>> Overall Grade: ${grade} % (${totalCorrect} of 5 responses correct) <<<`)
   
+
+  if(grade >= 80) {
+    console.log(`>>> Status: PASSED <<<`)
+  } else{
+    console.log(`>>> Status: FAILED <<<`)
+  }
 
   return grade;
 }
